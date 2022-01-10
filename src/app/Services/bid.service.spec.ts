05f394ec4b1dd2b7891d32ee8ae4a58a55211ce4
Sandeep <sendtosandeeps@gmail.com>
2022-01-10 22:32:41 +0530

    modified

--- src/app/Services/bid.service.spec.ts
+++ src/app/Services/bid.service.spec.ts
@@ -1,16 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BidService } from './bid.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('BidService', () => {
   let service: BidService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ BidService]
+    });
     service = TestBed.inject(BidService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a collection of products', () => {
+    const prodResponse = [{
+      id: "fsad6673Zuwewe",
+      productName: "Chain",
+      category: "Ornaments",
+      startingPrice: 100,
+      bidEndDate: new Date("20/01/2022")
+    }];
+
+    let response;
+
+    spyOn(service, 'getAllProducts').and.returnValues(of(prodResponse));
+    service.getAllProducts().subscribe(x=>{
+      response=x;
+    })
+    expect(response).toEqual(prodResponse);
+  });
+
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('EAuctionPortal');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('EAuctionPortal app is running!');
-  });
+  
 });
